be3edff1dafe57c18222d9b36a56fab1480dc7b6
[1O16] <youiiyou1253@gmail.com>
2022-10-22 23:00:31 +0900

    useSpotify

---
+++ hooks/useSpotify.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useSession, signIn } from "next-auth/react";
+import spotifyApi from "../lib/spotify";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+});
+
+export default function useSpotify() {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      // 만약 refresh access token을 얻는데 실패하면 login 페이지로 direct
+      if (session.error === "RefreshAccessTokenError") {
+        signIn();
+      }
+
+      spotifyApi.setAccessToken(session.user.accessToken);
+    }
+  }, [session]);
+
+  return spotifyApi;
+}
