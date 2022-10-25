e7e90e50daec0d7c60fcf32af3694103ad6c0e22
[1O16] <youiiyou1253@gmail.com>
2022-10-26 01:29:44 +0900

    recoil

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   );
 }
 
 export default MyApp;
+
+// 2:20:15
