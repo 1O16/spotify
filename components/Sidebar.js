79675d276dc4cefd12ee772cc3674ec4cb1d2384
[1O16] <youiiyou1253@gmail.com>
2022-10-31 01:56:04 +0900

    ml

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -33,7 +33,7 @@ function Sidebar() {
   return (
     <div
       className="text-gray-500 bg-black p-5 text-xs lg:text-sm border-r border-gray-900 
-    overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem]"
+    overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex"
     >
       <div className="space-y-4">
         <button
